f75c4c3b42d8de07efe754467880bd8a8bcc3c01
David Martel <DavidKMartel@gmail.com>
2015-04-04 08:58:13 -0700

    Displays previously entered values for login

--- StartSession.php
+++ StartSession.php
@@ -3,6 +3,20 @@ include 'SourceRcon.php';
 include 'usefulFunctions.php';
 session_start();
 
+define("DEFAULT_ADDRESS","10.0.0.150");
+define("DEFAULT_PORT","27015");
+define("DEFAULT_PASSWORD","pass");
+
+
+if(!defined($_SESSION['address'])) {
+	$_SESSION['address'] = DEFAULT_ADDRESS;
+}
+if(!defined($_SESSION['port'])) {
+	$_SESSION['port'] = DEFAULT_PORT;
+}
+if(!defined($_SESSION['password'])) {
+	$_SESSION['password'] = DEFAULT_PASSWORD;
+}
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 	$_SESSION['address'] = $_POST["address"];
 	$_SESSION['port'] = $_POST["port"];
@@ -24,9 +38,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
 <form name="Login" method="POST">
 	<table>
-		<tr><td>Address:</td>	<td><input type="text" name="address"	value="192.168.1.15"></text></td></tr>
-		<tr><td>Port:</td>		<td><input type="text" name="port"		value="27015"></text></td></tr>
-		<tr><td>Password:</td>	<td><input type="text" name="password"	value="pass"></text></td></tr>
+		<tr><td>Address:</td>	<td><input type="text" name="address"	value="<?php echo $_SESSION['address'];?>"></text></td></tr>
+		<tr><td>Port:</td>		<td><input type="text" name="port"		value="<?php echo $_SESSION['port'];?>"></text></td></tr>
+		<tr><td>Password:</td>	<td><input type="text" name="password"	value="<?php echo $_SESSION['password'];?>"></text></td></tr>
 		<tr><td><input type="submit" name="submit" value="Login"></td></tr>
 	</table>
 </form>
